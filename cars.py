2cd0f334ab8e492975de16d77432bb29f00968f9
Omkar Pawar <omkarpawar@Omkars-MacBook-Pro.local>
2020-05-09 21:29:55 -0500

    Function declaration

---
+++ cars.py
@@ -0,0 +1,33 @@
+# Define a class for the car store
+
+class carStore:
+
+    # Initialize the store with number of cars available
+    def __init__(self , number_of_cars = 0)
+
+    # See how many cars are available in store
+    def show_number_of_cars(self)
+
+    # Rent car on hourly basis
+    def rent_hourly(self , required_cars)
+
+    # Rent car on daily basis
+    def rent_daily(self, required_cars)
+
+    # Rent car on weekly basis
+    def rent_weekly(self , required_cars)
+
+    # When the car is returned back
+    def return_car(self , car_rent_details)
+
+
+class Customer:
+
+    # Initialize Customer details
+    def __init(self)
+
+    # Make request to the store
+    def car_request(self)
+
+    # Return car back to the store
+    def car_return(self)
