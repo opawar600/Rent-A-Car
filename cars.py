29a33dca29b251ec3e6a3db32113c591aa4cc80d
Omkar Pawar <omkarpawar@Omkars-MacBook-Pro.local>
2020-05-11 00:41:45 -0500

    Defined billing function when customer returns a car. Some changes still neede

--- cars.py
+++ cars.py
@@ -8,43 +8,54 @@ class carStore:
 
     def valid_car_count(self,n):
         if n <= 0:
-            print("Please enter a valid car number")
+            print("\nPlease enter a valid car number\n")
             return False
 
         elif n > self.available_cars:
-            print("The number of cars that you requested is more than available cars.")
+            print("\nThe number of cars that you requested is more than available cars.\n")
+            exit()
             return False
         else:
             return True
 
     # See how many cars are available in store
     def show_number_of_cars(self):
-        print("Thanks for checking upon carRentals. \nCurrently we have {} cars available to rent".format(self.available_cars))
+        print("\nThanks for checking upon carRentals. \nCurrently we have {} cars available to rent\n".format(self.available_cars))
 
     # Rent car on hourly basis
     def rent_hourly(self, required_cars):
         # Check if the number of cars specified is valid
         if self.valid_car_count(required_cars):
-            print("You have rented {} Car(s) on hourly basis".format(required_cars))
-            print("You will be charged $9 per hour for each car.")
+            print("\nYou have rented {} Car(s) on hourly basis\n".format(required_cars))
+            print("\nYou will be charged $9 per hour for each car.\n")
             self.available_cars -= required_cars
 
     # Rent car on daily basis
     def rent_daily(self, required_cars):
         if self.valid_car_count(required_cars):
-            print("You have rented {} Car(s) on daily basis".format(required_cars))
-            print("You will be charged $22 per day for each car.")
+            print("\nYou have rented {} Car(s) on daily basis\n".format(required_cars))
+            print("\nYou will be charged $22 per day for each car.\n")
             self.available_cars -= required_cars
 
     # Rent car on weekly basis
     def rent_weekly(self, required_cars):
         if self.valid_car_count(required_cars):
-            print("You have rented {} Car(s) on weekly basis".format(required_cars))
-            print("You will be charged $100 per week for each car.")
+            print("\nYou have rented {} Car(s) on weekly basis\n".format(required_cars))
+            print("\nYou will be charged $100 per week for each car.\n")
             self.available_cars -= required_cars
 
     # When the car is returned back
-    #def return_car(self, car_rent_details)
+    def return_car(self, customer_details):
+        # Fill the inventory
+        self.available_cars += customer_details.cars_rented
+
+        # Calculate the bill
+        bill = 0
+        if customer_details.rentType == 1:
+            bill = (customer_details.cars_rented * customer_details.rentPeriod * 9)
+            print("\nYou had rented {} car(s) on daily basis".format(customer_details.cars_rented))
+            print("\nYour bill is {}".format(bill))
+
 
 
 class Customer:
@@ -61,14 +72,18 @@ class Customer:
         self.invoice = 0
 
     # Make request to the store
+    # Takes an object of carStore to update the carStore inventory
     def car_request(self):
-        n = int(input("Enter the number of cars you wish to rent"))
+        n = int(input("\nEnter the number of cars you wish to rent\n"))
 
         if n < 1:
-            print ("Please enter a valid number of cars you want to rent")
+            print ("\nPlease enter a valid number of cars you want to rent\n")
+            return 0
         else:
+            # update the number of cars rented by customer
             self.cars_rented = n
-            print("Thank you! You will get {} Car(s) as per your request".format(n))
+            return n
+
 
 
 
